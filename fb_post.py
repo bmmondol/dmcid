4a8b71872a9f73179be32053b4e2dbed7432d110
bmmondol <137624536+bmmondol@users.noreply.github.com>
2025-12-04 19:57:29 +0600

    Update fb_post.py

--- fb_post.py
+++ fb_post.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 import os
@@ -8,37 +9,39 @@ cookie_raw = os.getenv("FB_COOKIE")
 group_link = os.getenv("GROUP_LINK")
 post_text = os.getenv("POST_TEXT")
 
-# --- Configure Chrome ---
 opts = Options()
 opts.add_argument("--headless=new")
 opts.add_argument("--no-sandbox")
 opts.add_argument("--disable-dev-shm-usage")
 
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=opts)
+# FIXED Chrome initialization
+service = Service(ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service, options=opts)
 
-# Open Facebook
 driver.get("https://www.facebook.com")
 
-# Convert cookie string → selenium cookie
-if cookie_raw:
-    for part in cookie_raw.split("; "):
+# Add cookies
+for part in cookie_raw.split("; "):
+    try:
         key, val = part.split("=", 1)
         driver.add_cookie({"name": key, "value": val})
+    except:
+        pass
 
 driver.get(group_link)
 time.sleep(5)
 
-# Post Box find
+# Find and click "Write something" box
 textarea = driver.find_element("xpath", "//div[contains(@aria-label,'Write something')]")
 textarea.click()
 time.sleep(2)
 
-# Type message
+# Type text
 post_box = driver.find_element("xpath", "//div[@role='textbox']")
 post_box.send_keys(post_text)
 time.sleep(2)
 
-# Click Post Button
+# Click post
 post_btn = driver.find_element("xpath", "//div[@aria-label='Post']")
 post_btn.click()
 
